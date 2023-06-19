edb5bd3e462716d339c45c7d8aba5993446a938e
rxMATTEO <erik_sundberg@mail.ru>
2023-06-19 14:11:38 +0300

    Eslint configure, title change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './index.sass';
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>This is example text just to make sure everything works fine</h1>

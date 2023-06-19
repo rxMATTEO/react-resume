354c0db1cc37dd5266f465c4ab66c903a1a0b791
rxMATTEO <erik_sundberg@mail.ru>
2023-06-19 15:49:32 +0300

    Sidebar component create

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './index.sass';
+import Sidebar from "./components/Sidebar";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
       <h1>This is example text just to make sure everything works fine</h1>
+      <Sidebar/>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import './sidebar.sass';
+
+
+function Sidebar() {
+  return (
+    <>
+      <header className="sidebar-left">
+
+      </header>
+    </>
+  );
+}
+
+export default Sidebar;

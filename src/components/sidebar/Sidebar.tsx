0f282873387231f3d6a04930d82204b0d72bd919
rxMATTEO <erik_sundberg@mail.ru>
2023-06-20 15:35:47 +0300

    Header to nav

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,12 +4,12 @@ import './sidebar.sass';
 function Sidebar() {
   return (
     <>
-      <header className="sidebar-left">
+      <nav className="sidebar-left">
         <div className="top">
           <div className="image"><img src="./static/about-me.jpg" alt="avatar"/></div>
           <span className="image-label">@volodin-artem</span>
         </div>
-      </header>
+      </nav>
     </>
   );
 }

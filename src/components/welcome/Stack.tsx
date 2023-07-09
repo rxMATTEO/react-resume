fd8e1f52e9a7891f4143ff6199bce2555d958463
rxMATTEO <erik_sundberg@mail.ru>
2023-07-09 17:35:27 +0300

    Add header to stack

--- src/components/welcome/Stack.tsx
+++ src/components/welcome/Stack.tsx
@@ -1,15 +1,18 @@
-import CardWithImage from '../inputs/CardWithImage';
+import CardWithImage from '../inputs/CardWithImage.tsx';
 
 function Stack() {
   return (
-    <div className="grid grid-cols-2 grid-rows-3 gap-5">
-      <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
-      <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
-      <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
-      <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
-      <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
-      <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
-    </div>
+    <>
+      <p className="text-5xl font-light uppercase tracking-[5px] mb-10">Мой стэк</p>
+      <div className="grid grid-cols-2 grid-rows-3 gap-5">
+        <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
+        <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
+        <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
+        <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
+        <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
+        <CardWithImage header="Web design" label="" mainText="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita, inventore non nulla repellat voluptas. Asperiores debitis deleniti doloremque ea error molestias omnis quisquam ratione, reprehenderit veniam. Architecto, explicabo, neque!" middleLabel="" imgPath="" />
+      </div>
+    </>
   );
 }
 

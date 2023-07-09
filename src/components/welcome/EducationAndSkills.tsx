8d80d31d3cad7df83d33cc933fb5ee6d39c371a0
rxMATTEO <erik_sundberg@mail.ru>
2023-07-09 15:21:10 +0300

    Add custom font

--- src/components/welcome/EducationAndSkills.tsx
+++ src/components/welcome/EducationAndSkills.tsx
@@ -5,12 +5,14 @@ function EducationAndSkills() {
     <>
       <p className="text-5xl font-light uppercase tracking-[5px] mb-10">Образование и навыки</p>
       <div className="flex flex-row mx-auto place-content-between">
-        <div className="education w-1/3">
+        <div className="education w-2/5">
           <CardWithLabel header="Graphic Designer" label="2017-2021" mainText="International Design Institute" />
           <CardWithLabel header="Graphic Designer" label="2017-2021" mainText="International Design Institute" />
           <CardWithLabel header="Graphic Designer" label="2017-2021" mainText="International Design Institute" />
         </div>
-        <div className="skills w-2/3" />
+        <div className="w-2/5 skills relative right-0">
+          <h1>Мои навыки</h1>
+        </div>
       </div>
     </>
   );

50f082291c6c1fc3e639f84f332a6ac5f8f9c8d9
rxMATTEO <erik_sundberg@mail.ru>
2023-07-22 21:03:00 +0300

    Set react masonry to work

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,16 +1,18 @@
+import Masonry from 'react-masonry-css';
+import './masonry.sass';
+
 function Portfolio() {
   return (
     <div className="mx-auto">
       <p className="text-5xl font-light uppercase tracking-[5px] mb-10">Мое портфолио</p>
-      <div className="grid grid-cols-3 gap-2">
+      <Masonry breackpointCols={5} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
         <div className="h-64 bg-purple" />
         <div className="h-36 bg-purple" />
         <div className="h-96 bg-purple" />
-
         <div className="h-96 bg-purple" />
         <div className="h-36 bg-purple" />
         <div className="h-24 bg-purple" />
-      </div>
+      </Masonry>
     </div>
   );
 }

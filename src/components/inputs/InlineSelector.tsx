d147aab08f378dc9bbc861fc8b64b5df44194b8e
rxMATTEO <erik_sundberg@mail.ru>
2023-07-23 00:27:33 +0300

    Add selector component for options

---
+++ src/components/inputs/InlineSelector.tsx
@@ -0,0 +1,29 @@
+import React, { DetailedReactHTMLElement, ReactElement, useState } from 'react';
+
+type InlineSelectorProps = {
+  children: ReactElement | ReactElement[],
+  containerClassName?: string,
+}
+
+function InlineSelector({ children, containerClassName }: InlineSelectorProps) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  function onSelect(index) {
+    setSelectedIndex(index);
+  }
+  return (
+    <div className={`inline-flex ${containerClassName}`}>
+      {React.Children.map(children, (child, index) => React.cloneElement(child as DetailedReactHTMLElement<any, any>, {
+        onClick: () => {
+          onSelect(index);
+        },
+        className: `${child.props.className} ${index === selectedIndex ? 'selected' : ''}`,
+      }))}
+    </div>
+  );
+}
+
+InlineSelector.defaultProps = {
+  containerClassName: '',
+};
+
+export default InlineSelector;
--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import Isotope from 'isotope-layout';
 import './masonry.sass';
+import InlineSelector from '../inputs/InlineSelector.tsx';
 
 function Portfolio() {
   const isotope = useRef(null);
@@ -53,7 +54,13 @@ function Portfolio() {
   return (
     <div className="mx-auto">
       <p className="text-5xl font-light uppercase tracking-[5px] mb-10">Мое портфолио</p>
-      <div className="isotope">
+      <InlineSelector containerClassName="filters">
+        <div className="filter mr-5">Все</div>
+        <div className="filter mr-5">Языки</div>
+        <div className="filter mr-5">Frontend</div>
+        <div className="filter mr-5">Backend</div>
+      </InlineSelector>
+      <div className="isotope mt-5">
         <div className="h-64 filterable"><div className="bg-purple h-full">1</div></div>
         <div className="h-36 filterable node"><div className="bg-purple h-full">node</div></div>
         <div className="h-96 filterable node"><div className="bg-purple h-full">node</div></div>

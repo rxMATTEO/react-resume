d6df34affc6dfd02f3d812a108b9e7ba1358c622
rxMATTEO <erik_sundberg@mail.ru>
2023-09-06 15:12:50 +0300

    Add sidebar link

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ function Sidebar() {
       <nav className="sidebar-left sm:w-[10%]">
         <div className="top">
           <div className="image"><img src="./static/about-me.jpg" alt="avatar" /></div>
-          <span className="image-label">@rxMATTEO</span>
+          <a className="image-label text-purple" href="https://linktr.ee/rxMATTEO" target="_blank" rel="noreferrer">@rxMATTEO</a>
         </div>
         <NavLinks />
         <SocialLinks />

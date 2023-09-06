22900ef809a94107e317257e1ba15d171138dec4
rxMATTEO <erik_sundberg@mail.ru>
2023-09-06 09:50:11 +0300

    Add images to stack component, change some description

--- src/components/contactme/ContactMe.tsx
+++ src/components/contactme/ContactMe.tsx
@@ -6,12 +6,12 @@ function ContactMe() {
   return (
     <div>
       <p className="text-5xl font-light uppercase tracking-[5px] mb-10">Контакты</p>
-      <div className="flex justify-center gap-5 py-5">
-        <a target="_blank" href="https://t.me/rxmatteo" rel="noreferrer">
-          <Button icon={<FaTelegram />} text="Telegram" color="!bg-blue-400 hover:!text-white" />
-        </a>
+      <div className="flex justify-center gap-5 py-5 flex-wrap">
         <a target="_blank" href="https://github.com/rxmatteo" rel="noreferrer">
           <Button icon={<AiFillGithub />} text="Github" color="!bg-neutral-600 hover:!text-white" />
+        </a>
+        <a target="_blank" href="https://t.me/rxmatteo" rel="noreferrer">
+          <Button icon={<FaTelegram />} text="Telegram" color="!bg-blue-400 hover:!text-white" />
         </a>
         <a target="_blank" href="https://hh.ru/here-link-to-resume" rel="noreferrer">
           <Button icon={<span className="bg-white text-xs rounded-full text-red-500 p-1">hh</span>} text="HeadHunter" color="!bg-red-500 hover:!text-white" />
--- src/components/welcome/Stack.tsx
+++ src/components/welcome/Stack.tsx
@@ -5,14 +5,14 @@ function Stack() {
     <div id="stack">
       <p className="text-5xl font-light uppercase tracking-[5px] mb-10">Мой стэк</p>
       <div className="grid lg:grid-cols-2 grid-cols-1 grid-rows-3 gap-5">
-        <CardWithImage header="JavaScript/Typescript" label="" mainText="Тесты на Jest, стили на Tailwind" middleLabel="" imgPath="" />
-        <CardWithImage header="HTML/CSS" label="" mainText="Адаптивная верстка, flex, grid, mobile first, препроцессор SASS, bootstrap 5" middleLabel="" imgPath="" />
-        <CardWithImage header="React" label="" mainText="Redux (старый и toolkit), React Query, Router" middleLabel="" imgPath="" />
-        <CardWithImage header="Vue" label="" mainText="Nuxt 3, Vue router, pinia, Prime vue, Prime flex" middleLabel="" imgPath="" />
-        <CardWithImage header="Node.JS" label="" mainText="Express, Sequelize, Prisma" middleLabel="" imgPath="" />
-        <CardWithImage header="Сборщики" label="" mainText="Webpack, vite" middleLabel="" imgPath="" />
-        <CardWithImage header="Контроль версий" label="" mainText="Git, gitlab" middleLabel="" imgPath="" />
-        <CardWithImage header="Прочее" label="" mainText={'IDE: PHPstorm. \nЛинтеры: ESlint, prettier'} middleLabel="" imgPath="" />
+        <CardWithImage header="JavaScript/Typescript" label="" mainText="Typescript 5+, Vanilla JS, DOM." middleLabel="" imgPath="https://github.com/devicons/devicon/raw/master/icons/typescript/typescript-plain.svg" />
+        <CardWithImage header="Vue / Nuxt" label="" mainText="Nuxt 3, Vue 3 (TSX, рендер-функции, шаблоны), Vue router, Pinia, PrimeVue, PrimeFlex" middleLabel="" imgPath="https://github.com/devicons/devicon/raw/master/icons/vuejs/vuejs-original.svg" />
+        <CardWithImage header="React" label="" mainText="Redux (старый и toolkit), React Query, Router" middleLabel="" imgPath="https://github.com/devicons/devicon/raw/master/icons/react/react-original-wordmark.svg" />
+        <CardWithImage header="HTML/CSS" label="" mainText="Адаптивная верстка, flex, grid, mobile first, SASS, Tailwind CSS" middleLabel="" imgPath="https://github.com/devicons/devicon/raw/master/icons/sass/sass-original.svg" />
+        <CardWithImage header="Node.JS" label="" mainText="Express, Sequelize, Prisma" middleLabel="" imgPath="https://github.com/devicons/devicon/raw/master/icons/nodejs/nodejs-plain.svg" />
+        <CardWithImage header="Сборщики" label="" mainText="Vite, Webpack" middleLabel="" imgPath="https://camo.githubusercontent.com/a845cf08bc585efdc903b5b4734cbc0623967d16216446d8c9c2fa299edfde34/68747470733a2f2f766974656a732e72752f6c6f676f2e737667" />
+        <CardWithImage header="Контроль версий" label="" mainText="Git, Gitlab" middleLabel="" imgPath="https://github.com/devicons/devicon/raw/master/icons/gitlab/gitlab-original.svg" />
+        <CardWithImage header="Прочее" label="" mainText={'IDE: PHPstorm. \nЛинтеры: ESlint, prettier'} middleLabel="" imgPath="https://camo.githubusercontent.com/5c6f6d20c5e35e76e8e663040ca89c35af5b670d05b3069507bc560fc59c76c2/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f72752f7468756d622f632f63382f2544302539422544302542452544302542332544302542452544312538322544302542382544302542465f50687053746f726d2e7376672f3132303070782d2544302539422544302542452544302542332544302542452544312538322544302542382544302542465f50687053746f726d2e7376672e706e67" />
       </div>
     </div>
   );
